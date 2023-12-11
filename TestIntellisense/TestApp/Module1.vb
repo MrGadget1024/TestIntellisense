9a62964cd479157876302aa75c32db3a01312f6c
MrGadget1024 <9826063+MrGadget1024@users.noreply.github.com>
2023-12-11 12:12:47 -0500

    Solution demonstrating EditorBrowsable Never doesn't work

---
+++ TestIntellisense/TestApp/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+	Sub Main()
+		Dim test As New TestClass.TestClass
+
+		' This property should be visible in the IDE
+		test.VisibleProperty = "Visible"
+
+		' This method should be visible in the IDE
+		test.VisibleMethod()
+
+		' This property should be hidden in the IDE
+		test.HiddenProperty = "Hidden"
+
+		' This method should be hidden in the IDE
+		test.HiddenMethod()
+
+	End Sub
+
+End Module
---
+++ TestIntellisense/TestClass/TestClass.vb
@@ -0,0 +1,17 @@
+﻿Imports System.ComponentModel
+
+Public Class TestClass
+
+	Public Property VisibleProperty As String
+
+	Public Sub VisibleMethod()
+	End Sub
+
+	<EditorBrowsable(EditorBrowsableState.Never)>
+	Public Property HiddenProperty As String
+
+	<EditorBrowsable(EditorBrowsableState.Never)>
+	Public Sub HiddenMethod()
+	End Sub
+
+End Class
